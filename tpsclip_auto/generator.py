b6e65c44fd135eea5080377a66d51ee8dd54af6f
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-30 19:41:20 +0700

    Update generator.py

--- tpsclip_auto/generator.py
+++ tpsclip_auto/generator.py
@@ -1,1 +1,7 @@
-
+def generate_commands(dom_structure):
+    commands = []
+    for inp in dom_structure['inputs']:
+        commands.append(f"fill_field('{inp}', '<value>')")
+    for btn in dom_structure['buttons']:
+        commands.append(f"click_button('{btn}')")
+    return commands
